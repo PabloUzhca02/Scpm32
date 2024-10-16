dc4959ebbb1c65d1500a96069962cc1a92bb31c1
PabloUzhca02 <uzhcapablo03@gmail.com>
2024-10-15 20:53:52 -0500

    Add files via upload
    Codigo para poder sacar una Mac para el scp-32

---
+++ Mac.py
@@ -0,0 +1,17 @@
+import network
+
+
+
+wlan = network.WLAN(network.STA_IF)
+
+wlan.active(True)
+
+
+if wlan.active():
+
+        mac_address - wlan.config("mac")
+        print(mac_address)
+        print("Device MAC Address:", ":".join(["{:02X}".format(byte) for byte in mac_address]))
+else:
+    print("WI-FI is not active.")
+    
